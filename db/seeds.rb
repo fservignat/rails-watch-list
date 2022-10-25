ff5787c99b2f6a2171141add5433f6b701eb4e6b
Francis Servignat <francis.servignat@outlook.com>
2022-10-25 18:52:30 +0200

    modified gemfile, added images to file, added scss design, seeded and created db, ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Cleaning db..."
+Movie.destroy_all
+puts "Cleaned up"
+
+20.times do
+  puts "Adding a movie"
+  movie = Movie.new(title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+    rating: [1,2,3,4,5].sample)
+  movie.save!
+  puts "Movie #{movie.title} created"
+end
+
+puts "#{Movie.count} movies created"
